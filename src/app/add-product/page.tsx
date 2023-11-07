04847eb0f0d87db6b4df8dbdc49366e2866e9b8f
FUSIONWORKSHOP\Alan.Boyle <alan.boyle@unrvld.com>
2023-11-07 14:47:40 +0000

    Form to add new products to DB

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,14 +1,65 @@
+import FormSubmitButton from "@/components/FormSubmitButton";
+import { prisma } from "@/lib/db/prima";
+import { redirect } from "next/navigation";
+
+export const metadata = {
+  title: "Add Product",
+};
+
+async function AddProduct(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+
+  if (!name || !description || !imageUrl || !price) {
+    throw Error("Missing Required Fields");
+  }
+
+  await prisma.product.create({
+    data: { name, description, imageUrl, price },
+  });
+
+  redirect("/");
+}
+
 export default function AddProductPage() {
-    return (
-        <div>
-            <h1 className="mb-3 text-lg font-bold">Add Product</h1>
-            <form>
-                <input
-                    required
-                    name="name"
-                    placeholder="Name"
-                    className="input-bordered input mb-3 w-full" />
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="mb-3 text-lg font-bold">Add Product</h1>
+      <form action={AddProduct}>
+        <input
+          required
+          name="name"
+          placeholder="Name"
+          className="input-bordered input mb-3 w-full"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="Description"
+          className="textarea-bordered textarea mb-3 w-full"
+        />
+        <input
+          required
+          name="imageUrl"
+          placeholder="Image Url"
+          type="url"
+          className="input-bordered input mb-3 w-full"
+        />
+        <input
+          required
+          name="price"
+          placeholder="Price"
+          type="number"
+          className="input-bordered input mb-3 w-full"
+        />
+        <FormSubmitButton className="btn-block">
+          Add Project
+        </FormSubmitButton>
+      </form>
+    </div>
+  );
 }
